60fa45052016eecce16d70c3a6e2e87a9f524f53
Caleb Norton <n0603919@outlook.com>
2024-08-15 14:19:59 -0500

    Undo symlinking

--- MP5/src_bonus/copykernel.sh
+++ MP5/src_bonus/copykernel.sh
@@ -1,1 +1,8 @@
-../../common/copykernel.sh
+attach=$(hdiutil attach dev_kernel_grub.img)
+mountpoint=${attach#* }
+# trim leading and trailing whitespace
+mountpoint=$(echo $mountpoint | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+device=${attach%% *}
+cp kernel.elf "$mountpoint"
+sleep 1
+hdiutil detach "$device" > /dev/null
--- common/copykernel.sh
+++
@@ -1,8 +0,0 @@
-attach=$(hdiutil attach dev_kernel_grub.img)
-mountpoint=${attach#* }
-# trim leading and trailing whitespace
-mountpoint=$(echo $mountpoint | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
-device=${attach%% *}
-cp kernel.elf "$mountpoint"
-sleep 1
-hdiutil detach "$device" > /dev/null
